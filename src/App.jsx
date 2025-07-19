8fa191f9f9180b8b5854921f066db3bb706ec747
Mamta Epili <mamtaepili5@gmail.com>
2025-07-19 19:59:48 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <h1>HELLO VITE + REACT</h1>
+      <h1>HELLO VITE REACT</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
